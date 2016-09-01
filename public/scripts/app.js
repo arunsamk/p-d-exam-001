a2300c1898154d2752be928f8eec8b0a0df5d574
ArunKSamson <arunsamk@gmail.com>
2016-09-01 18:21:38 +0530

    007

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,7 +8,7 @@
  *
  * Main module of the application.
  */
-angular.module('examApp', [
+var exampApplication = angular.module('examApp', [
     'ngResource',
     'ngRoute'   
   ])
@@ -20,9 +20,7 @@ angular.module('examApp', [
         controllerAs: 'main'
       })
     .when('/contact', {
-        templateUrl:'views/contact.html'
-        
-        
+        templateUrl:'views/contact.html'               
     })
     .when('/about' , {
         templateUrl : 'views/aboutus.html'
@@ -37,16 +35,52 @@ angular.module('examApp', [
    /*when('/profile', {
         templateUrl : 'modalContainer',
         controller : 'ProfileModalCtrl'
-    })*/
-      
+    })*/      
 .otherwise({
         redirectTo: '/'
-      });
-    
-     
-
+      });     
   });
 /*app.controller('ProfileModalCtrl', function($scope, $modal) {
     $modal.open({templateUrl:'modal.html'});
 });
 */
+
+exampApplication.controller('questionCtrl', ['$scope', '$http', function($scope, $http){
+
+    //when landing on the page get all the rows in the collection
+    $http.get('/api/questions').success(function(data){
+      $scope.questions = data;
+      console.log(data);
+    }).error(function(data){
+      console.log('Error' + data);
+    });
+
+    //when submitting the add form, send the text node api
+    $scope.createQuestion = function(){
+      $http.post('/api/questions', $scope.formData).success(function(data){
+        $scope.formData = {};
+        $scope.questions = data;
+        console.log(data);      
+      }).error(function(data){
+        console.log('Error :' + data);
+      });
+
+    };
+
+    // Conditional Listing
+    $scope.listQuestions = function(){
+      var parameters = {
+        QuestCateg: $scope.formData.QuestCateg,
+        QuestDCateg: $scope.formData.QuestDCateg
+      };  
+      var config = {
+        params: parameters
+      };
+      $http.get('/api/questions', config).success(function(data){
+        $scope.questions = data;
+        console.log('From dbcore.js --> ' + data);
+      }).error(function(data){
+        console.log('Error ' + data);
+      });
+    };
+  }]);
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 
 var express = require('express');
 var application = express();
-/*var mongoose  = require('mongoose');
+var mongoose  = require('mongoose');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var path = require('path');
 var methodOverride = require('method-override');
 var Schema = mongoose.Schema;
-*/
+
 //Making server file to assign port dynamically.
 var port = process.env.PORT || 7010;
-/*
+
 // Connecting to MongoDB
 var connection = mongoose.connect('mongodb://localhost/crkappi');
 
@@ -25,17 +25,114 @@ var questionSchema = new Schema({
 
 //Defining a model in mongoose
 var Question = mongoose.model('dummyQuestions', questionSchema);
-*/
+
 //-----------------------Configuration---------------------------------------
 
 //Making express to look in the public directory for (css, js, html .....).
 application.use(express.static(__dirname + '/public'));
-/*application.use(morgan('dev'));
+application.use(morgan('dev'));
 application.use(bodyParser.urlencoded({ 'extended': true }));
 application.use(bodyParser.json());
 application.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 application.use(methodOverride());
-*/
+
+//routes----------------------------
+//get all Questions
+application.get('/api/questions', function(request, response){
+
+	// conditional selecting
+	//Acquring records with specific Question Category and Question Difficulty Level
+	console.log('Hello from Get request in server.js');
+	if( request.query.QuestCateg && request.query.QuestDCateg ){
+		Question.find({ QuestCateg : request.query.QuestCateg, QuestDCateg : request.query.QuestDCateg }, function(err,questions){
+
+			//In case of error return the error to response.
+			if(err)
+				response.send(err);
+			response.json(questions);
+		});
+	}else{
+
+		//Acquring records with specific Question Category
+		if( request.query.QuestCateg ){			
+			Question.find( { QuestCateg : request.query.QuestCateg }, function(err, questions){
+
+				//In case of error return the error to reponse
+				if(err)
+					response.send(err);
+				response.json(questions);
+			});
+		}else{
+			//Acquring All records from MongoDB
+			Question.find(function(err, questions){
+
+				//In case of error return the error to reponse
+				if(err)
+					response.send(err);
+				response.json(questions);
+			});
+		}
+	}
+});
+
+// Create question and send back all question after creation
+
+application.post('/api/questions', function(request, response){
+
+	// Create a question, information comes from ajax request from angular
+	if ( request.body.QuestCateg && request.body.QuestDCateg && request.body.QuestTxt && request.body.AnswerTxt){
+		console.log('A ok for new Insertion into mongoDB');
+		Question.create({
+			QuestCateg : request.body.QuestCateg,
+			QuestDCateg : request.body.QuestDCateg,
+			QuestTxt : request.body.QuestTxt,
+			AnswerTxt : request.body.AnswerTxt,
+			done : false
+		}, function(err, questions){
+			if(err)
+				response.send(err);
+
+			//Repopulating the view after insert a new row in the collection
+
+			Question.find(function(err, questions){
+
+				//if there is an error retrieving data, send the error.
+
+				if(err)
+					response.send(err);
+				response.json(questions);
+			});
+		});
+	}
+	
+
+});
+
+//deleting a quesion
+
+application.delete('/api/questions/:question_id', function(request, response){
+
+	// deleting or removing a question based on the id.
+
+	Question.remove({
+		_id : request.params.question_id
+	},function(err, question){
+		if(err)
+			response.send(err);
+
+		//get and return all questions after deletion of a question
+
+		Question.find(function(err, questions){
+
+			//if error retrieving data, send the error.
+
+			if(err)
+				response.send(err);
+			response.json(questions);
+		});
+	});
+});
+
 //Setting the home page
 application.get('*', function(request, response){
 	response.sendFile('index');
