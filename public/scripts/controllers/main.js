c8218571011064e1904472f20912e7e36ce911f9
ArunKSamson <arunsamk@gmail.com>
2016-09-08 17:35:31 +0530

    08-09-2016-kudwi

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -10,57 +10,36 @@
 var eApplication = angular.module('examApp');
 
 //Factory to transfer value to qbank.js file
-eApplication.factory('questSelectservice', ['$scope', function($scope){
-	console.log('Getting Inside Factory');
-	var choice = '';	
-	var swQt = setQt;
-	console.log('Value of swQt ' + swQt);
-	switch(swQt){
-		case 'R':
-		choice = 'R';
-		break;
-		case 'SAS':
-		choice = 'SAS';
-		break;
-		case 'ML':
-		choice = 'ML';
-		break;
-		case 'Tableau':
-		choice = 'Tableau';
-		break;
-		case 'SQL':
-		choice = 'SQL';
-		break;
-		default:
-		choice='';
-		break;
-	}
-	console.log('Selected Value ' + choice);
-	return choice;
-}]);
+eApplication.factory('shareQuestionService', function ($rootScope) {
+	var sharedQuestion = {};
+	sharedQuestion.transferChoice = function(choice){
+		this.choice = choice;
+		console.log('Value acquired in choice in factory ' + choice);
+	};
+	return sharedQuestion;
+});
 
 //MainCtrl 
-eApplication.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
-	$scope.setQt = '';
-	 $scope.rQuestion = function(){
-	 	$scope.setQt = 'R';	 
-	 	console.log('setQt ' + $scope.setQt);
-	 };
-	 $scope.mLearn=function(){
-	 	$scope.setQt = 'ML';
-	 	console.log('ML');
-	 };
-	 $scope.tabLeau=function(){
-	 	$scope.setQt = 'Tableau';
-	 	console.log('Tableau');
-	 };
-	 $scope.sqlQuestion=function(){
-	 	$scope.setQt = 'SQL';
-	 	console.log('SQL');
-	 };
-	 $scope.sasQuestion=function(){
-	 	$scope.setQt = 'SAS';
-	 	console.log('SAS');
-	 };	  
+eApplication.controller('MainCtrl', ['$scope', 'shareQuestionService', function ($scope, sharedQuestion) {
+	$scope.rQuestion = function(){
+		var choice = 'R';
+		//console.log('Value of Choice in controller ' + choice);
+		sharedQuestion.transferChoice(choice);
+	};
+	$scope.mLearn = function(){
+		var choice = 'ML';
+		sharedQuestion.transferChoice(choice);
+	};
+	$scope.tabLeau = function(){
+		var choice = 'Tableau';
+		sharedQuestion.transferChoice(choice);
+	};
+	$scope.sqlQuestion = function(){
+		var choice = 'SQL';
+		sharedQuestion.transferChoice(choice);
+	};
+	$scope.sasQuestion = function(){
+		var choice = 'SAS';
+		sharedQuestion.transferChoice(choice);
+	};
 }]);
-
--- public/scripts/controllers/qbank.js
+++ public/scripts/controllers/qbank.js
@@ -1,20 +1,96 @@
 var eApplication = angular.module('examApp');
 
-eApplication.controller('qbankCtrl', ['$scope', '$http', 'questSelectservice', function ($scope, $http, questSelectservice) {
+eApplication.controller('qbankCtrl', ['$scope', '$http', 'shareQuestionService', function ($scope, $http, shareQuestionService) {
+
+	// Choice of Question Category from previous page and based on the choice populating values.
 	//console.log('Triggerring question bank controller');
-	console.log('Value passed in service' + questSelectservice);
-	var choice = questSelectservice;
-	console.log('value of Choice  ' + choice);
-	/* var parameters = {
-		QuestCateg: questSelectservice.choice
-	};  
+	//console.log('Value passed in service' + shareQuestionService);
+	var choice = shareQuestionService.choice;
+	//console.log('value of Choice in qbankCtrl  ' + choice);
+	var parameters = {
+		QuestCateg: choice
+	};
 	var config = {
 		params: parameters
 	};
 	$http.get('/api/questions', config).success(function(data){
-		$scope.questions = data;
-		 console.log('From dbcore.js --> ' + data);
-		 }).error(function(data){
-		 console.log('Error ' + data);
-		 });	 */
+		$scope.questions = data;		
+	}).error(function(data){
+		console.log('Error ' + data);
+	});
+
+	//Based on user selection 'R' values are populated from the question bank.
+	$scope.rQuestion = function(){
+		var parameters = {
+			QuestCateg: 'R'
+		};
+		var config = {
+			params: parameters
+		};
+		$http.get('/api/questions', config).success(function(data){
+			$scope.questions = data;
+		}).error(function(data){
+			console.log('Error ' + data);
+		});
+	};
+
+	//Based on user selection 'ML' values are populated from the question bank.
+	$scope.mlQuestion = function(){
+		var parameters = {
+			QuestCateg: 'ML'
+		};
+		var config = {
+			params: parameters
+		};
+		$http.get('/api/questions', config).success(function(data){
+			$scope.questions = data;
+		}).error(function(data){
+			console.log('Error ' + data);
+		});
+	};
+
+	//Based on user selection 'Tableau' values are populated from the question bank.
+	$scope.tabQuestion = function(){
+		var parameters = {
+			QuestCateg: 'Tableau'
+		};
+		var config = {
+			params: parameters
+		};
+		$http.get('/api/questions', config).success(function(data){
+			$scope.questions = data;
+		}).error(function(data){
+			console.log('Error ' + data);
+		});
+	};
+
+	//Based on user selection 'SQL' values are populated from the question bank.
+	$scope.sqlQuestion = function(){
+		var parameters = {
+			QuestCateg: 'SQL'
+		};
+		var config = {
+			params: parameters
+		};
+		$http.get('/api/questions', config).success(function(data){
+			$scope.questions = data;
+		}).error(function(data){
+			console.log('Error ' + data);
+		});
+	};
+
+	//Based on user selection 'SAS' values are populated from the question bank.
+	$scope.sasQuestion = function(){
+		var parameters = {
+			QuestCateg: 'SAS'
+		};
+		var config = {
+			params: parameters
+		};
+		$http.get('/api/questions', config).success(function(data){
+			$scope.questions = data;
+		}).error(function(data){
+			console.log('Error ' + data);
+		});
+	};
 }]);
